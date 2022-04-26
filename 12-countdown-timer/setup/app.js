890fba54724d94935bebabe71660cab0d71b5bdb
Motus Pilobolus <kayla.prata@gmail.com>
2022-04-25 22:10:45 -0400

    complete exercise 12

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,60 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+let futureDate = new Date(tempYear,tempMonth,tempDay + 10,0,0,0)
+
+// let futureDate = new Date(2022,4,6,18,30,0);
+
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}pm`
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = t/oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour );
+  let minutes = Math.floor((t % hours) / oneMinute );
+  let seconds = Math.floor((t % oneMinute) / 1000 );
+
+  const values = [days,hours,minutes,seconds];
+
+  function format(item) {
+    if (item < 10){
+      return item = `0${item}`
+    }
+    return item;
+  }
+  items.forEach(function(item,index){
+    item.innerHTML = format(values[index]);
+  })
+  if (t < 0){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has ended</h4>`
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
