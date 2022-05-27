ba2f0fae8369346c56db420c5098cb157cc730ae
Motus Pilobolus <kayla.prata@gmail.com>
2022-05-27 12:57:55 -0400

    complete exercise 35

--- 35-color-generator-r/src/App.js
+++ 35-color-generator-r/src/App.js
@@ -4,7 +4,42 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('');
+  const [error,setError] = useState(false);
+  const [list,setList] = useState(new Values("#ff66ff").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  }
+
+  return (
+  <>
+  <section className='container'>
+    <h3>color generator</h3>
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={color} 
+      onChange={(e) => setColor(e.target.value)}
+      placeholder='#ff66ff'
+      className={`${error?'error':null}`}
+      />
+      <button className='btn' type='submit'>
+        submit
+      </button>
+    </form>
+  </section>
+  <section className='colors'>
+    {list.map((color,index) => {
+      return <SingleColor key={index} {...color} index={index}/>
+    })}
+  </section>
+  </> )
 }
 
 export default App
